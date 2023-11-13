dc88dd0d56774d8678a4bc4d3730f86979f1efe1
Роман Назмиев <roma.nazmiev@gmail.com>
2023-11-13 14:07:25 +0700

    feat(HomeSectionGetTipsByCard): grid styles

--- app/ui/HomeSectionGetTipsByCard/index.tsx
+++ app/ui/HomeSectionGetTipsByCard/index.tsx
@@ -10,13 +10,17 @@ export default function HomeSectionGetTipsByCard() {
                     <p>Besplatno za lokale. Bez poreza. Novac </p>
                     <p>se prenosi direktno na račun zaposlenog. Gosti mogu da ocene uslugu i napišu komentare.</p>
                 </article>
-                <Image
-                    src="/hvala-banner-704.webp"
-                    alt="Get the tip by credit card"
-                    width={288}
-                    height={228} />
-                <a className={styles.startGettingTipsButton}>Počni da dobijaš baksis</a>
-                <a href="/restaurant/connect_page?reg=request">Registrujte svoj lokal</a>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/hvala-banner-704.webp"
+                        alt="Get the tip by credit card"
+                        width={704}
+                        height={558} />
+                </div>
+                <div className={styles.buttons}>
+                    <a className={styles.startGettingTipsButton}>Počni da dobijaš baksis</a>
+                    <a href="/restaurant/connect_page?reg=request">Registrujte svoj lokal</a>
+                </div>
             </div>
         </section>
     )
