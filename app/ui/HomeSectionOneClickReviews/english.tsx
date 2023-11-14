e19f54ce4bfcf4a8adf11898d57581ca753d107f
Рома <57133971+nazmiev@users.noreply.github.com>
2023-11-14 18:41:22 +0700

    feat(home): new section styles

--- app/ui/HomeSectionOneClickReviews/english.tsx
+++ app/ui/HomeSectionOneClickReviews/english.tsx
@@ -5,34 +5,36 @@ export default function HomeSectionOneClickReviewsEn() {
     return (
         <section className={styles.HomeSectionOneClickReviews}>
             <h2>One-click public reviews</h2>
-            <div className={styles.iPhoneImageContainer}>
-                <Image
-                    src="/OneClickReviewsIphone.png"
-                    alt="Public reviews in your phone"
-                    width={1293}
-                    height={2592} />
-            </div>
-            <div className={styles.imagesContainer}>
-                <div className={styles.googleImageContainer}>
+            <div className={styles.iphonebackdropContainer}>
+                <div className={styles.iPhoneImageContainer}>
                     <Image
-                        src="/OneClickReviewsGoogle.png"
+                        src="/OneClickReviewsIphone.png"
                         alt="Public reviews in your phone"
-                        width={2848}
-                        height={1392} />
+                        width={1293}
+                        height={2592} />
                 </div>
-                <div className={styles.instaImageContainer}>
-                    <Image
-                        src="/OneClickReviewsInsta.png"
-                        alt="Public reviews in your phone"
-                        width={1352}
-                        height={2388} />
-                </div>
-                <div className={styles.tripImageContainer}>
-                    <Image
-                        src="/OneClickReviewsTrip.png"
-                        alt="Public reviews in your phone"
-                        width={2560}
-                        height={714} />
+                <div className={styles.imagesContainer}>
+                    <div className={styles.tripImageContainer}>
+                        <Image
+                            src="/OneClickReviewsTrip.png"
+                            alt="Public reviews in your phone"
+                            width={2560}
+                            height={714} />
+                    </div>
+                    <div className={styles.googleImageContainer}>
+                        <Image
+                            src="/OneClickReviewsGoogle.png"
+                            alt="Public reviews in your phone"
+                            width={2848}
+                            height={1392} />
+                    </div>
+                    <div className={styles.instaImageContainer}>
+                        <Image
+                            src="/OneClickReviewsInsta.png"
+                            alt="Public reviews in your phone"
+                            width={1352}
+                            height={2388} />
+                    </div>
                 </div>
             </div>
         </section>
